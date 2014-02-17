4414485d574170776eee337e5aeed61053ddb2d6
hdknr <gmail@hdknr.com>
2014-02-17 12:10:47 +0900

    PHP related...

--- lib/makanai/centos.py
+++ lib/makanai/centos.py
@@ -1,5 +1,19 @@
 from fabric.api import task, run
 from base import *
+
 @task
 def packages():
     run('rpm -qa')
+
+@task
+def yum_update():
+    run('sudo yum update --exclude=kernel* --exclude=grub*')
+
+@task
+def yum_check():
+    run('sudo yum check-update --exclude=kernel* --exclude=grub*')
+
+@task
+def version():
+    run('more /etc/redhat-release')
+    run('uname -a')
